7b573fb59a70262ec01f277bf71804a9b48ab069
Flavio Carmona <32350974+fcarmonab@users.noreply.github.com>
2022-08-06 13:16:37 -0600

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,6 +2,7 @@
 usuario=$(whoami)
 opcion=1
 clear
+sudo su
 
 echo "---------- MENU INSTALACION DE SUITECRM ----------"
 echo "=================================================="
@@ -9,12 +10,10 @@ echo ""
 echo "Siga los siguientes pasos en el orden establecido:"
 echo "--------------------------------------------------"
 echo "1. Instalacion de GIT"
-echo "2. Descarga de archivos SuiteCRM"
-echo "3. Instalacion de Composer"
-echo "4. Instalacion de Apache"
-echo "5. Instalacion de php7.4 y modulos adicionales"
-echo "6. Instalacion de MySQL"
-echo "7. Configuraciones adicionales"
+echo "2. Instalacion de php y modulos adicionales"
+echo "3. Instalacion de Apache"
+echo "4. Instalacion de MySQL"
+echo "5. Configuraciones adicionales"
 echo "0. Salir"
 echo ""
 echo "SELECCIONE OPCION:"
@@ -26,48 +25,35 @@ while [ $opcion -ne 0 ]; do
         1)
           clear
           echo "Iniciando instalacion de GIT";
-          sudo apt-get update
-          sudo apt-get install git -y
+          apt-get update
+          apt-get install git -y
           clear
           opcion=0
           ;;
         2)
           clear
-          echo "Iniciando descarga de ficheros y directorios de SuiteCRM:"
-          sudo su
+          echo "Iniciando descarga de SuiteCRM e Instalacion de Composer:"
+          mkdir tmp
+          cd /tmp/
           sudo git clone https://github.com/salesagility/SuiteCRM.git suitecrm
-          sudo chmod -R 777 suitecrm
-          sudo mv suitecrm /var/www/
+          clear
+          ls
+          sleep 6
+          mv suitecrm /var/www/
           cd /var/www/
-          sudo chown -R www-data.www-data suitecrm
-          sudo chmod -R 775 suitecrm
           clear
-          opcion=0
-          ;;
-        3)
+          ls
+          sleep 6
+          chown -R www-data.www-data suitecrm
+          chmod -R 775 suitecrm
+          apt-get install composer -y
+          cd suitecrm/
+          composer install
           clear
-          echo "Iniciando instalacion de composer:"
-          sudo apt-get install composer -y
-          cd /var/www/suitecrm/
-          sudo composer install
-          clear
-          opcion=0
-          ;;
-        4)
-          clear
-          echo "Instalando Apache:"
-          sudo apt-get install apache2 -y
-          clear
-          opcion=0
-          ;;
-        5)
-          clear
-          sudo cp php.ini temp1.ini
           sudo apt remove php8*
           sudo apt-get purge php*
           sudo apt-get autoremove
           sudo apt-get autoclean
-          sudo cp temp1.ini php.ini
           sudo add-apt-repository ppa:ondrej/php
           sudo apt-get update
           sudo apt-get install php7.4 -y
@@ -76,7 +62,14 @@ while [ $opcion -ne 0 ]; do
           clear
           opcion=0
           ;;
-        6)
+        3)
+          clear
+          echo "Instalando Apache:"
+          sudo apt-get install apache2 -y
+          clear
+          opcion=0
+          ;;
+        4)
           clear
           sudo apt install mysql-server -y
           sudo mysql -u root < script.sql
@@ -84,21 +77,8 @@ while [ $opcion -ne 0 ]; do
           clear
           opcion=0
           ;;
-        7)
+        5)
           clear
-          cd /home/ubuntu/sistemasAbiertos
-          sudo cp php.ini temp1.ini
-          sudo chmod -R 777 php.ini
-          sudo mv php.ini /etc/php/7.4/apache2/php.ini
-          sudo cp temp1.ini php.ini
-          sudo chmod -R 777 php.ini
-          sudo systemctl restart apache2
-          sudo cp suitecrm.conf temp2.conf
-          sudo chmod -R 777 suitecrm.conf
-          sudo mv suitecrm.conf /etc/apache2/sites-available/suitecrm.conf
-          sudo cp temp2.conf suitecrm.conf
-          sudo chmod -R 777 suitecrm.conf
-          sudo systemctl restart apache2
           sudo a2ensite suitecrm.conf
           sudo systemctl restart apache2
           sudo a2dissite 000-default.conf
