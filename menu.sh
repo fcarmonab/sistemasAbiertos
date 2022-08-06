7fd83a74df394d98afe96f7ae70322db00139941
Flavio Carmona <32350974+fcarmonab@users.noreply.github.com>
2022-08-06 11:06:22 -0600

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 usuario=$(whoami)
-opcion=11
+opcion=1
 clear
 
 echo "---------- MENU INSTALACION DE SUITECRM ----------"
@@ -29,22 +29,20 @@ while [ $opcion -ne 0 ]; do
           sudo apt-get update
           sudo apt-get install git -y
           clear
-          opcion=2  
+          opcion=0
           ;;
         2)
           clear
           echo "Iniciando descarga de ficheros y directorios de SuiteCRM:";
           sudo su
-          cd ~
-          mkdir tmp
-          cd /tmp/
+          cd sistemasAbiertos
           sudo git clone https://github.com/salesagility/SuiteCRM.git suitecrm
           mv suitecrm /var/www/
           cd /var/www/
           sudo chown -R www-data.www-data suitecrm
           sudo chmod -R 775 suitecrm
           clear
-          opcion=3
+          opcion=0
           ;;
         3)
           clear
@@ -53,14 +51,14 @@ while [ $opcion -ne 0 ]; do
           cd suitecrm/
           composer install
           clear
-          opcion=4
+          opcion=0
           ;;
         4)
           clear
           echo "Instalando Apache:"
           sudo apt-get install apache2 -y
           clear
-          opcion=5
+          opcion=0
           ;;
         5)
           clear
@@ -70,7 +68,7 @@ while [ $opcion -ne 0 ]; do
           sudo apt-get install php7.4-cli php7.4-common php7.4-pgsql php7.4-json php7.4-opcache php7.4-mysql php7.4-mbstring php7.4-fpm php7.4-intl php7.4-simplexml  php7.4-gd php7.4-mcrypt php7.4-soap php7.4-bcmath libapache2-mod-php7.4 php7.4-xml php7.4-common php7.4-cli php7.4-curl php7.4-intl php7.4-xmlrpc php7.4-json php7.4-zip zip unzip -y
           sudo systemctl restart apache2
           clear
-          opcion=6
+          opcion=0
           ;;
         6)
           clear
@@ -78,7 +76,7 @@ while [ $opcion -ne 0 ]; do
           sudo mysql -u root < script.sql
           sudo systemctl restart apache2
           clear
-          opcion=7
+          opcion=0
           ;;
         7)
           clear
